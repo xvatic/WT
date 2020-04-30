5d0454eed5cc44edc63270434f9ad1f285cd5e14
xvatic <glazkovE@gmail.com>
2020-04-30 15:22:40 +0300

    navigation bar added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <title>AIRHYPE</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="grid-item-header">
+          <?php include ('navbar.php') ?>
+           
+        </div>
+        <div class="grid-item-main-content">
+
+            
+            <?php include (''.$page.'.php') ?>
+        </div>
+
+        
+       
+         
+        
+    </div>
+</body>
+</html>
